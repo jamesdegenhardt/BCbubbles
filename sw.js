709324a913a0ced49d3b001c4e607432b6745828
James <jameslidegenhardt@proton.me>
2026-09-19 14:43:36 -0400

    added bot names

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bcbubbles-v3';
+const CACHE_NAME = 'bcbubbles-v4';
 const APP_SHELL = [
   './',
   './index.html',
@@ -21,9 +21,9 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+  event.respondWith(fetch(event.request).then((response) => {
     const copy = response.clone();
     caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
     return response;
-  }).catch(() => event.request.mode === 'navigate' ? caches.match('./index.html') : Promise.reject(new Error('Resource unavailable')))));
+  }).catch(() => caches.match(event.request).then((cached) => cached || (event.request.mode === 'navigate' ? caches.match('./index.html') : Promise.reject(new Error('Resource unavailable'))))));
 });
